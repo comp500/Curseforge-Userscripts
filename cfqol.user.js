2fbd9b6bc490ec9ef3fd9e8182e3de3f2a522b4c
comp500 <comp500@users.noreply.github.com>
2019-09-03 03:19:01 +0100

    Reformat

--- cfqol.user.js
+++ cfqol.user.js
@@ -15,19 +15,23 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	"use strict";
 
-    // Change the Browse link and the default Minecraft tab (from other links) to /minecraft/mc-mods
-    let regexBrowse = /^http:\/\/bit.ly\/2Lzpfsl|https:\/\/www.curseforge.com\/minecraft\/?$/;
-    Array.from(document.getElementsByTagName("a")).filter(a => regexBrowse.test(a.href)).forEach(a => {a.href = "https://www.curseforge.com/minecraft/mc-mods"});
+	// Change the Browse link and the default Minecraft tab (from other links) to /minecraft/mc-mods
+	let regexBrowse = /^http:\/\/bit.ly\/2Lzpfsl|https:\/\/www.curseforge.com\/minecraft\/?$/;
+	Array.from(document.getElementsByTagName("a"))
+		.filter(a => regexBrowse.test(a.href))
+		.forEach(a => {
+			a.href = "https://www.curseforge.com/minecraft/mc-mods";
+		});
 
-    // Add a search box
-    let searchBoxContainer = document.createElement("div");
-    searchBoxContainer.className = "flex mr-4 items-center";
+	// Add a search box
+	let searchBoxContainer = document.createElement("div");
+	searchBoxContainer.className = "flex mr-4 items-center";
 	// Get the current assets path
 	let styleSheet = Array.from(document.styleSheets).find(s => /\/Content\//.test(s.href));
 	let assetsPath = styleSheet == null ? "2-0-7179-35052" : /\/Content\/([\d\-]+)\//.exec(styleSheet.href)[1];
-    searchBoxContainer.innerHTML = `<form action="/minecraft/mc-mods/search" method="get" novalidate="novalidate" autocomplete="false" style="width:100%">
+	searchBoxContainer.innerHTML = `<form action="/minecraft/mc-mods/search" method="get" novalidate="novalidate" autocomplete="false" style="width:100%">
     <div class="flex flex-col h-full justify-between">
          <div class="input input--icon" style="color: #000">
             <i class="search textgray-900 flex items-center justify-center">
@@ -36,14 +40,14 @@
             <input type="text" name="search" id="cfqolTopbarSearch" placeholder="Search Mods">
         </div>
     </div></form>`;
-    let insertLocation = document.querySelector(".private-message");
-    if (insertLocation != null) {
-        // @Inject(method = "the navbar", at = @At("HEAD"))
-        insertLocation.parentNode.insertBefore(searchBoxContainer, insertLocation);
+	let insertLocation = document.querySelector(".private-message");
+	if (insertLocation != null) {
+		// @Inject(method = "the navbar", at = @At("HEAD"))
+		insertLocation.parentNode.insertBefore(searchBoxContainer, insertLocation);
 	}
 
 	// Make the search box magically grow
-    let searchBox = searchBoxContainer.querySelector("#cfqolTopbarSearch");
+	let searchBox = searchBoxContainer.querySelector("#cfqolTopbarSearch");
 	if (searchBox != null) {
 		// Fix stupid flexboxes - set to flex-grow 1 flex-shrink 0
 		searchBoxContainer.style.flex = "1 0";
@@ -72,26 +76,33 @@
 
 	// Hide useless links, to save space
 	let uselessLinks = ["Minecraft Forums"];
-	Array.from(document.querySelectorAll(".top-nav__nav-link")).filter(n => uselessLinks.includes(n.innerText)).forEach(n => n.parentNode.removeChild(n));
+	Array.from(document.querySelectorAll(".top-nav__nav-link"))
+		.filter(n => uselessLinks.includes(n.innerText))
+		.forEach(n => n.parentNode.removeChild(n));
 
-    // Add an "All Files" tab
-    let pathMatches = /\/minecraft\/mc-mods\/([a-z][\da-z\-_]{0,127})/.exec(document.location.pathname);
-    let files = document.getElementById("nav-files");
-    if (pathMatches != null && pathMatches.length == 2 && files != null) {
-        let slug = pathMatches[1];
-        let allFiles = document.createElement("li");
-        let isAllFilesPage = /\/minecraft\/mc-mods\/[a-z][\da-z\-_]{0,127}\/files\/all/.test(document.location.pathname);
-        if (isAllFilesPage) {
-            allFiles.className = "border-b-2 border-primary-500 b-list-item p-nav-item px-2 pb-1/10 -mb-1/10 text-gray-500";
-            files.className = "b-list-item p-nav-item px-2 pb-1/10 -mb-1/10 text-gray-500";
-        } else {
-            allFiles.className = "b-list-item p-nav-item px-2 pb-1/10 -mb-1/10 text-gray-500";
-        }
-        allFiles.innerHTML = `<a href="/minecraft/mc-mods/${slug}/files/all" class="text-${isAllFilesPage ? "primary" : "gray"}-500 hover:no-underline">
+	// Add an "All Files" tab
+	let pathMatches = /\/minecraft\/mc-mods\/([a-z][\da-z\-_]{0,127})/.exec(document.location.pathname);
+	let files = document.getElementById("nav-files");
+	if (pathMatches != null && pathMatches.length == 2 && files != null) {
+		let slug = pathMatches[1];
+		let allFiles = document.createElement("li");
+		let isAllFilesPage = /\/minecraft\/mc-mods\/[a-z][\da-z\-_]{0,127}\/files\/all/.test(
+			document.location.pathname
+		);
+		if (isAllFilesPage) {
+			allFiles.className =
+				"border-b-2 border-primary-500 b-list-item p-nav-item px-2 pb-1/10 -mb-1/10 text-gray-500";
+			files.className = "b-list-item p-nav-item px-2 pb-1/10 -mb-1/10 text-gray-500";
+		} else {
+			allFiles.className = "b-list-item p-nav-item px-2 pb-1/10 -mb-1/10 text-gray-500";
+		}
+		allFiles.innerHTML = `<a href="/minecraft/mc-mods/${slug}/files/all" class="text-${
+			isAllFilesPage ? "primary" : "gray"
+		}-500 hover:no-underline">
             <span class="b-list-label">
                 All Files
             </span>
         </a>`;
-        files.parentNode.insertBefore(allFiles, files.nextSibling);
-    }
+		files.parentNode.insertBefore(allFiles, files.nextSibling);
+	}
 })();
