c1642af291f8ae0bbc614d9229e0d7b0d778b676
comp500 <comp500@users.noreply.github.com>
2020-12-30 23:36:09 +0000

    Bump version

--- cfqol.user.js
+++ cfqol.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Curseforge QOL Fixes
-// @version      0.18
+// @version      0.19
 // @description  Various Quality of Life improvements to the Curseforge website
 // @author       comp500
 // @namespace    https://infra.link/
